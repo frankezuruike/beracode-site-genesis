b43e87ca413990beaff5df21fabd3c6aeed2589d
frankezuruike <70523485+frankezuruike@users.noreply.github.com>
2025-08-30 20:19:54 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { HelmetProvider } from "react-helmet-async";
 
 const queryClient = new QueryClient();
 
@@ -23,8 +22,7 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
-      </HelmetProvider>
-    </TooltipProvider>
+      </TooltipProvider>
   </QueryClientProvider>
 );
 
